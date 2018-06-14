1f5ea1eab07cc6d5647441607aaa74c3e97dc1fa
Sankalp Chauhan <sankalpchauhan.me@gmail.com>
2018-06-14 11:31:37 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,10 +4,10 @@ def fizzbuzz(int)
   if int % 3 == 0
     "Fizz" # Go fizz
   end
-  elsif int % 5 == 0
+  else if int % 5 == 0
     "Buzz"
   end
-  elsif int % 3 == 0 && int % 5 == 0
+  else if int % 3 == 0 && int % 5 == 0
     "FizzBuzz"
   end
   else
